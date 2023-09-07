cddd33aa42f3dc31499264d453fb764f0e45757f
Rod Vagg <rod@vagg.org>
2023-09-07 10:20:45 +1000

    feat: fork graphsync/testutils components we need to avoid networking deps

---
+++ testutil/chaintypes/doc.go
@@ -0,0 +1,4 @@
+// Package chaintypes is a fork of some components of
+// github.com/ipfs/go-graphsync/testutil/chaintypes in order to avoid networking
+// dependencies imposed by Graphsync.
+package chaintypes
